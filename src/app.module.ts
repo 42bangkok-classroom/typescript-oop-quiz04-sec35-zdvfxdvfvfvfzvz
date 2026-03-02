cbe678b5c0f0fabd3e560a3e2ac1c7ed546f3069
zdvfxdvfvfvfzvz <68010756@kmitl.ac.th>
2026-03-02 10:15:27 +0700

    ggg

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserInterface } from './user/user.interface';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserService, UserInterface],
 })
 export class AppModule {}
